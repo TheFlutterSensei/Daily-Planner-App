ca2690e4044b8ab7ebc2136ed075059df503a910
Flutter Sensei <hi@fluttersensei.com>
2026-02-04 10:09:18 +0530

    Displaying notes on HomeScreen, addedd Delete functionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
+import 'package:daily_planner_app/helpers/datetime_helper.dart';
+import 'package:daily_planner_app/models/note.dart';
 import 'package:daily_planner_app/screens/add_note.dart';
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +12,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<Note> _notes = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +22,48 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
       ),
+      body: _notes.isEmpty
+          ? const Center(child: Text('No notes added yet'))
+          : GroupedListView<Note, DateTime>(
+              elements: _notes,
+              groupBy: (note) => note.dateOnly,
+              groupSeparatorBuilder: (date) => Padding(
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  formatDate(date),
+                  style: TextStyle(fontWeight: FontWeight.w500),
+                ),
+              ),
+              itemBuilder: (context, note) {
+                return ListTile(
+                  tileColor: Theme.of(context).colorScheme.surfaceContainer,
+                  title: Text(note.title),
+                  subtitle: Text(
+                    formateTime(note.time),
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  trailing: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _notes.remove(note);
+                      });
+                    },
+                    icon: Icon(Icons.delete),
+                  ),
+                );
+              },
+            ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (_) => AddNote()));
+        onPressed: () async {
+          final newNote = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => AddNote()),
+          );
+          if (newNote != null) {
+            setState(() {
+              _notes.add(newNote);
+            });
+          }
         },
         shape: CircleBorder(),
         child: const Icon(Icons.add),
